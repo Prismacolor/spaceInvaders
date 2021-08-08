8c3bdde6863ae93a1094572d4b9e3d11967dec87
TeeRoberts <taylornroberts2009@gmail.com>
2021-08-08 11:40:24 -0500

    adding tests

--- tests/game_items_test.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-
-if __name__ == '__main__':
-
-    with open('items_test.log', 'w') as f:
-        trunner = unittest.TextTestRunner(f)
-        unittest.main(testRunner=trunner)
